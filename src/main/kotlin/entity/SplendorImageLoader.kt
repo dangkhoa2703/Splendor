da2ba0e7e834e182b805ef8a567e4e54374d0716
Justin Schartner <justin.schartner@tu-dortmund.de>
2022-03-09 20:16:04 +0100

    FileChooser in LoadGameScene.kt

--- src/main/kotlin/entity/SplendorImageLoader.kt
+++ src/main/kotlin/entity/SplendorImageLoader.kt
@@ -5,6 +5,7 @@ import javax.imageio.ImageIO
 import tools.aqua.bgw.visual.ImageVisual
 
 private const val HUMAN_ICON = "/human_green.jpg"
+private const val DRAG_N_DROP_ICON = "/dragAndDrop.png"
 
 class SplendorImageLoader {
 	fun humanIcon(): ImageVisual {
@@ -12,5 +13,12 @@ class SplendorImageLoader {
 			SplendorImageLoader::class.java.getResource(HUMAN_ICON)
 		)
 		return ImageVisual(image)
+	}
+
+	fun dragAndDrop(): ImageVisual {
+	    val image: BufferedImage = ImageIO.read(
+		SplendorImageLoader::class.java.getResource(DRAG_N_DROP_ICON)
+	    )
+	    return ImageVisual(image)
 	}
 }
--- src/main/kotlin/view/LoadGameScene.kt
+++ src/main/kotlin/view/LoadGameScene.kt
@@ -6,8 +6,15 @@ import tools.aqua.bgw.components.uicomponents.Label
 import tools.aqua.bgw.util.Font
 import tools.aqua.bgw.visual.ColorVisual
 import tools.aqua.bgw.components.uicomponents.Button
+import javafx.stage.FileChooser
+import java.io.File
+import entity.SplendorImageLoader
+import tools.aqua.bgw.visual.ImageVisual
 
 class LoadGameScene(private val rootService: RootService): MenuScene(1920, 1080), Refreshable {
+
+    private val fileChooser = FileChooser()
+    private var openedFileChooser: Boolean = false
 
     private val headLineLabel = Label(
 	width = 300, height = 200,
@@ -15,6 +22,35 @@ class LoadGameScene(private val rootService: RootService): MenuScene(1920, 1080)
 	text = "Load Game",
 	font = Font(size = 44)
     )
+
+    private val areaLabel = Label(
+	width = 400, height = 400,
+	posX = width/2 - 200, posY = height/2 - 200,
+	text = ""
+    )
+
+    private val chooseFileButton = Button(
+	width = 100, height = 50,
+	posX = width/2 - 50, posY = height/2 - 25,
+	text = "loadFile", font = Font(size=16),
+	visual = ColorVisual(255, 255, 255)
+    ).apply{
+	onMouseClicked = {
+	    if(!openedFileChooser) {
+		openedFileChooser = true
+		val file: File? = fileChooser.showOpenDialog(null)
+		openedFileChooser = false
+		if(file!=null) println(file.getName())
+	    }
+	}
+    }
+
+    private val startButton = Button(
+	width = 200, height = 100,
+	posX = 1650, posY = 930,
+	text = "Start", font = Font(size=18),
+	visual = ColorVisual(136, 221, 136)
+    ) 
 
     val backButton = Button(
 	width = 200, height = 100,
@@ -26,9 +62,19 @@ class LoadGameScene(private val rootService: RootService): MenuScene(1920, 1080)
     init{
 	background = ColorVisual(108, 168, 59)
 
+	val imageLoader = SplendorImageLoader()
+	val image: ImageVisual = imageLoader.dragAndDrop()
+
+	areaLabel.visual = image
+
+	startButton.isDisabled = true
+
 	addComponents(
 	    headLineLabel,
 	    backButton,
+	    startButton,
+	    chooseFileButton,
+	    areaLabel,
 	)
     }
 }
