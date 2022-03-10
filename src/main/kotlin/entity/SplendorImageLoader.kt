bfb3c2390b2cf6b11605562a1e0c97b6f3f40dc9
neele.hormann <neele.hormann@tu-dortmund.de>
2022-03-10 21:33:13 +0100

    SplendorImageLoader.kt Wiederherstellung

--- src/main/kotlin/entity/SplendorImageLoader.kt
+++ src/main/kotlin/entity/SplendorImageLoader.kt
@@ -7,13 +7,13 @@ import tools.aqua.bgw.visual.ImageVisual
 private const val HUMAN_ICON = "/human_green.jpg"
 private const val DRAG_N_DROP_ICON = "/dragAndDrop.png"
 private const val START_BACKGROUND = "/Splendor-background.jpg"
+private const val BUTTON_IMAGE = "/button.jpg"
+private const val REDO_IMAGE = "/redo.png"
+private const val UNDO_IMAGE = "/Undo.png"
+private const val HINT_IMAGE = "/hint.png"
+private const val TABLE_IMAGE = "/Table.png"
 
-/**
- * 	class for loading the images of Splendor
- * */
 class SplendorImageLoader {
-
-	/** gets icons of players */
 	fun humanIcon(): ImageVisual {
 		val image: BufferedImage = ImageIO.read(
 			SplendorImageLoader::class.java.getResource(HUMAN_ICON)
@@ -21,7 +21,6 @@ class SplendorImageLoader {
 		return ImageVisual(image)
 	}
 
-	/** dragAndDrop */
 	fun dragAndDrop(): ImageVisual {
 		val image: BufferedImage = ImageIO.read(
 			SplendorImageLoader::class.java.getResource(DRAG_N_DROP_ICON)
@@ -29,21 +28,59 @@ class SplendorImageLoader {
 		return ImageVisual(image)
 	}
 
-	/** gets front Images for revealed cards */
 	fun frontImageFor(id: Int): ImageVisual {
 		val idString = (id+1).toString()
 		return ImageVisual(
 			ImageIO.read(
-				SplendorImageLoader::class.java.getResource("/cards/$idString.jpg")
+				SplendorImageLoader::class.java.getResource("/cards/"+idString+".jpg")
 			)
 		)
 	}
 
-	/** background */
 	fun startBackground(): ImageVisual {
 		return ImageVisual(
 			ImageIO.read(
 				SplendorImageLoader::class.java.getResource(START_BACKGROUND)
+			)
+		)
+	}
+
+	fun button(): ImageVisual {
+		return ImageVisual(
+			ImageIO.read(
+				SplendorImageLoader::class.java.getResource(BUTTON_IMAGE)
+			)
+		)
+	}
+
+	fun redoButton(): ImageVisual {
+		return ImageVisual(
+			ImageIO.read(
+				SplendorImageLoader::class.java.getResource(REDO_IMAGE)
+			)
+		)
+	}
+
+	fun hintButton(): ImageVisual {
+		return ImageVisual(
+			ImageIO.read(
+				SplendorImageLoader::class.java.getResource(BUTTON_IMAGE)
+			)
+		)
+	}
+
+	fun undoButton(): ImageVisual {
+		return ImageVisual(
+			ImageIO.read(
+				SplendorImageLoader::class.java.getResource(UNDO_IMAGE)
+			)
+		)
+	}
+
+	fun table(): ImageVisual {
+		return ImageVisual(
+			ImageIO.read(
+				SplendorImageLoader::class.java.getResource(TABLE_IMAGE)
 			)
 		)
 	}
