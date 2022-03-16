b4792f380cc2e73498caab474d1e86484cde57e7
Moritz Haneke <moritz.haneke@tu-dortmund.de>
2022-03-16 22:40:40 +0100

    Updated DecisionTree

--- src/main/kotlin/entity/TurnType.kt
+++ src/main/kotlin/entity/TurnType.kt
@@ -5,11 +5,9 @@ package entity
  *  @property TAKE_GEMS: take two gems of the same type or take maximum three gems of different types
  *  @property RESERVE_CARD: reserve one devCard and receive one yellow gem
  *  @property BUY_CARD: buy one devCard and receive its bonus and prestige points
- *  @property EMPTY: empty turn, for structural purposes
  */
 enum class TurnType {
     TAKE_GEMS,
     BUY_CARD,
-    RESERVE_CARD,
-    EMPTY
+    RESERVE_CARD
 }
