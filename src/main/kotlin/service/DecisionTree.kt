ad222d8d89d8e608e40c110f324f98249bf78d3f
Moritz Haneke <moritz.haneke@tu-dortmund.de>
2022-03-17 11:35:22 +0100

    Fixed Redo

--- src/main/kotlin/service/DecisionTree.kt
+++ src/main/kotlin/service/DecisionTree.kt
@@ -112,13 +112,11 @@ class DecisionTree(var rootService: RootService) {
      */
     fun simulateMove(turnType: TurnType, board: Board, player: Player, enemyPlayer: List<Player>):
             Pair<Turn, Pair<Board, Player>>? {
-        val newBoard = board.cloneForSimulation()
-        val newPlayer = player.clone()
         // No cards on the board
-        if((newBoard.levelOneOpen.size + newBoard.levelTwoOpen.size + newBoard.levelThreeOpen.size) <= 0)
+        if((board.levelOneOpen.size + board.levelTwoOpen.size + board.levelThreeOpen.size) <= 0)
             return null
         val bestDevCards: Map<DevCard, Double> = rootService.aiService
-            .calculateGeneralDevCardScore(newBoard, newPlayer, enemyPlayer)
+            .calculateGeneralDevCardScore(board, player, enemyPlayer)
         when (turnType) {
             TurnType.TAKE_GEMS -> {
                 val chosenGems: Pair<Map<GemType, Int>, Boolean> = rootService.aiService
@@ -129,11 +127,11 @@ class DecisionTree(var rootService: RootService) {
                 }
                 val turn = Turn(mapOfChosenGems, listOf(), TurnType.TAKE_GEMS, chosenGems.second)
                 // Update Board and Player
-                newBoard.gems = newBoard.gems.combine(mapOfChosenGems, subtract = true)
+                board.gems = board.gems.combine(mapOfChosenGems, subtract = true)
                 mapOfChosenGems.forEach {
-                    newPlayer.gems[it.key] = (newPlayer.gems[it.key] ?: 0) + it.value
+                    player.gems[it.key] = (player.gems[it.key] ?: 0) + it.value
                 }
-                return Pair(turn, Pair(newBoard, newPlayer))
+                return Pair(turn, Pair(board, player))
             }
             TurnType.BUY_CARD -> {
                 val cardsSortedAfterScore = bestDevCards.keys.sortedBy { bestDevCards[it] }
@@ -150,15 +148,15 @@ class DecisionTree(var rootService: RootService) {
                 }
                 // Update Board and player
                 val turn = Turn(mapOf(), listOf(affordableCards[0]), TurnType.BUY_CARD)
-                newBoard.levelOneOpen.remove(affordableCards[0])
-                newBoard.levelTwoOpen.remove(affordableCards[0])
-                newBoard.levelThreeOpen.remove(affordableCards[0])
-                newPlayer.devCards.add(affordableCards[0])
-                newPlayer.gems.clear()
-                newPlayer.gems.putAll(newPlayer.gems.combine(affordableCards[0].price, subtract = true))
-                newPlayer.bonus[affordableCards[0].bonus] = (newPlayer.bonus[affordableCards[0].bonus]?: 0) + 1
-                newPlayer.score += affordableCards[0].prestigePoints
-                return Pair(turn, Pair(newBoard, newPlayer))
+                board.levelOneOpen.remove(affordableCards[0])
+                board.levelTwoOpen.remove(affordableCards[0])
+                board.levelThreeOpen.remove(affordableCards[0])
+                player.devCards.add(affordableCards[0])
+                player.gems.clear()
+                player.gems.putAll(player.gems.combine(affordableCards[0].price, subtract = true))
+                player.bonus[affordableCards[0].bonus] = (player.bonus[affordableCards[0].bonus]?: 0) + 1
+                player.score += affordableCards[0].prestigePoints
+                return Pair(turn, Pair(board, player))
             }
             TurnType.RESERVE_CARD -> {
                 val cardsSortedAfterScore = bestDevCards.keys.sortedBy { bestDevCards[it] }
@@ -179,15 +177,16 @@ class DecisionTree(var rootService: RootService) {
                 }
                 // Update Board and player
                 val turn = Turn(mapOf(), listOf(reservedCards[0]), TurnType.RESERVE_CARD)
-                newBoard.levelOneOpen.remove(reservedCards[0])
-                newBoard.levelTwoOpen.remove(reservedCards[0])
-                newBoard.levelThreeOpen.remove(reservedCards[0])
-                newPlayer.reservedCards.add(reservedCards[0])
+                board.levelOneOpen.remove(reservedCards[0])
+                board.levelTwoOpen.remove(reservedCards[0])
+                board.levelThreeOpen.remove(reservedCards[0])
+                player.reservedCards.add(reservedCards[0])
                 if (board.gems[GemType.YELLOW] != 0) {
-                    newPlayer.gems[GemType.YELLOW]?.plus(1)
+                    player.gems[GemType.YELLOW]?.plus(1)
                 }
-                return Pair(turn, Pair(newBoard, newPlayer))
+                return Pair(turn, Pair(board, player))
             }
+            else -> return null
         }
     }
 
